59add7225eb942c82b549f9cd89dc0931745cad8
Vladimir Jancar <vlad.jancar@gmail.com>
2025-07-03 22:13:20 +0200

    Continued work on packet creation / parsing; state became a global variable

--- comm.py
+++ comm.py
@@ -3,19 +3,19 @@ import struct
 import threading
 from enum import Enum
 
-
-# PORT = 8880;PEER_PORT = 8888
-PORT = 8888;PEER_PORT = 8880
-
-PEER_IP = "127.0.0.1"
-BUFFER_SIZE = 1024
-MAX_FRAGMENT_SIZE = 1400
-
-
 class State(Enum):
     INPUT = 0
     HANDSHAKE = 1
     CONNECTED = 2
+
+# PORT = 8880;PEER_PORT = 8888
+PORT = 8888;PEER_PORT = 8880
+PEER_IP = "127.0.0.1"
+
+BUFFER_SIZE = 1024
+MAX_FRAGMENT_SIZE = 1400
+
+G_state = State.INPUT
 
 
 class Communication():
@@ -36,8 +36,8 @@ class Data():
     def __init__(self):
         pass
 
-    def encodePacket(self, data):
-        flags = (self.ack << 7) | (self.syn << 6) | (self.fin << 5) | (self.err << 4) | (self.sfs << 3) | (self.lfg << 2) | (self.ftr << 1)
+    def createPacket(self, ack_num: int, seq_num: int, ack=0, syn=0, fin=0, ctr=0, data: bytes) -> bytes:
+        flags = (self.ack << 7) | (self.syn << 6) | (self.fin << 5) #!| (self.err << 4) | (self.sfs << 3) | (self.lfg << 2) | (self.ftr << 1)
         checksum = self.calculateChecksum(data.encode('utf-8'))
 
         if (self.ftr==1 and self.seq_num==2 and CORRUPT):
@@ -45,7 +45,7 @@ class Data():
             CORRUPT = False
 
         header = struct.pack(
-            '!IIBH',
+            '!IIBH',#! SIZE
             self.ack_num,          # 32b
             self.seq_num,          # 32b
             flags,                 # 8b
@@ -53,25 +53,19 @@ class Data():
         )
         return header + data.encode('utf-8')
 
-    def decodePacket(self, packet):
+    def parsePacket(self, packet: bytes):
         header = packet[:11]
         ack_num, seq_num, flags, checksum = struct.unpack('!IIBH', header)
-        data = packet[11:].decode('utf-8')
-        return Packet(
-            ack_num=ack_num,
-            seq_num=seq_num,
-            ack=(flags >> 7) & 1,
-            syn=(flags >> 6) & 1,
-            fin=(flags >> 5) & 1,
-            err=(flags >> 4) & 1,
-            sfs=(flags >> 3) & 1,
-            lfg=(flags >> 2) & 1,
-            ftr=(flags >> 1) & 1,
-            checksum=checksum,
-            data=data
-        )
+        data = packet[11:].decode('utf-8') #! 11:
+        ack=(flags >> 7) & 1
+        syn=(flags >> 6) & 1
+        fin=(flags >> 5) & 1
+        # err=(flags >> 4) & 1
+        # sfs=(flags >> 3) & 1
+        # lfg=(flags >> 2) & 1
+        # ftr=(flags >> 1) & 1
 
-    def calculateChecksum(self, data):
+    def calculateChecksum(self, data: bytes):
         polynomial = 0x8005
         crc = 0xFFFF
 
@@ -89,7 +83,6 @@ class Data():
 
 
 def main():
-    state = State.INPUT
     src_ip = PEER_IP
     peer_ip = PEER_IP #input("[INPUT] peer IP address: ").strip()
     src_port = PORT #int(input("[INPUT] listening port: ").strip())
@@ -102,11 +95,11 @@ def main():
     #state = State.HANDSHAKE
     #TODO handshake
 
-    state = State.CONNECTED
+    G_state = State.CONNECTED
     
     while True:
-        if (state == State.CONNECTED):
-            sender(sock, peer_ip, peer_port)
+        if (G_state == State.CONNECTED):
+            handleInput(sock, peer_ip, peer_port)
 
 
 def receive(sock, src_ip, src_port):
@@ -117,9 +110,48 @@ def receive(sock, src_ip, src_port):
         print(f"{addr[0]}:{addr[1]} >> {data.decode()}")
 
 
-def sender(sock, peer_ip, peer_port):
-    msg = input("\n")
-    sock.sendto(msg.encode(), (peer_ip, peer_port))
+def handleInput(sock, peer_ip, peer_port):
+    user_input = input("\n")
+
+    #TODO
+    # if user_input.startswith("/setfragsize "):
+    #     try:
+    #         new_size = int(user_input.split()[1])
+    #         self.setFragmentSize(new_size)
+    #     except ValueError:
+    #         print("Invalid command. Usage: /setfragsize <size>")
+    # elif user_input.startswith("/send "):
+    #     file_path = user_input[6:].strip()
+    #     self.sendFile(file_path)
+    # elif user_input.startswith("/disconnect"):
+    #     self.trerminateConnection()
+    # else:
+    sock.sendto(user_input.encode(), (peer_ip, peer_port))
+
+
+# def sendText(self, message):
+#         self.message_seq_num = self.message_seq_num + 1
+#         if self.message_seq_num == 0: 
+#             self.message_seq_num = 1
+
+#         if message == "": return
+
+#         if len(message) > protocol.frag_size:
+#             fgs = protocol.fragmentData(message)
+#             frags = enumerate(fgs)
+#             for seq_num, fragment in frags:
+#                 if seq_num == len(fgs)-1:
+#                     packet = Packet(seq_num=self.message_seq_num, data=fragment, ftr=1, lfg=1)
+#                 else:
+#                     packet = Packet(seq_num=self.message_seq_num, data=fragment, ftr=1)
+
+#                 self.socket.sendto(packet.toBytes(), (self.dest_ip, self.dest_port))
+#                 self.message_seq_num += 1
+                
+#         else:
+#             packet = Packet(seq_num=self.message_seq_num, data=message)
+#             self.socket.sendto(packet.toBytes(), (self.dest_ip, self.dest_port))
+#             self.message_seq_num += 1
 
 
 if __name__ == "__main__":
